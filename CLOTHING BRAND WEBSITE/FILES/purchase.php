32e945afb375959bc170c5d58afcead5467774c0
Shah hussain <135989217+Shahhussain90@users.noreply.github.com>
2024-06-21 18:33:39 +0500

    Add files via upload

--- CLOTHING BRAND WEBSITE/FILES/purchase.php
+++ CLOTHING BRAND WEBSITE/FILES/purchase.php
@@ -1,6 +1,7 @@
 <?php
-include "config.php";
 session_start();
+include "config.php";
+
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -10,20 +11,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if (mysqli_query($con, $query_1)) {
 
+            $customer_order_id = mysqli_insert_id($con);
             $query_2 = "INSERT INTO `customer_order_details` (`customer_order_id`,`customer_order_Product_Name`, `customer_order_price`, `customer_order_size`, `customer_order_quantity`) VALUES (?,?,?,?,?)";
 
             $stmt = mysqli_prepare($con, $query_2);
 
             if ($stmt) {
-                mysqli_stmt_bind_param($stmt, 'isisi', $customer_order_id, $product_name, $product_price, $product_sizes, $product_quantity);
+                mysqli_stmt_bind_param($stmt,'isisi',$customer_order_id,$product_name,$product_price,$product_sizes,$product_quantity);
 
-                foreach ($_SESSION['cart'] as $key => $values) {
+                foreach($_SESSION['cart'] as $key => $values) {
                     $product_name = $values['product_name'];
                     $product_price = $values['product_price'];
                     $product_sizes = $values['product_sizes'];
                     $product_quantity = $values['product_quantity'];
 
                     mysqli_stmt_execute($stmt);
+                    error_reporting(0);
                 }
 
                 unset($_SESSION['cart']);
