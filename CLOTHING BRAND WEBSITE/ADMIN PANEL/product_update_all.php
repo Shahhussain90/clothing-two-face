78f78c72059b278a63fe5146d431f291b56ee321
Shah hussain <135989217+Shahhussain90@users.noreply.github.com>
2024-07-05 14:02:25 +0500

    Add files via upload

--- CLOTHING BRAND WEBSITE/ADMIN PANEL/product_update_all.php
+++ CLOTHING BRAND WEBSITE/ADMIN PANEL/product_update_all.php
@@ -14,9 +14,11 @@ if (isset($_POST['update_product'])) {
     $No_Discount_price = $_POST['No_Discount_price'];
     $product_description = $_POST['product_description'];
     $product_alt_text = $_POST['product_alt_text'];
+    $fabric = $_POST['fabric'];
     $product_image = $_FILES['product_image']['name'];
     $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
     $product_image_folder = 'uploaded_img/' . $product_image;
+
 
 
 
@@ -24,14 +26,14 @@ if (isset($_POST['update_product'])) {
        $message[]='please fill form';
     }
     else{
-        $update = "UPDATE `all_products` SET product_name='$product_name',product_price='$product_price',No_Discount_price='$No_Discount_price',product_description='$product_description',product_alt_text='$product_alt_text',product_image='$product_image'
+        $update = "UPDATE `all_products` SET product_name='$product_name',product_price='$product_price',No_Discount_price='$No_Discount_price',product_description='$product_description',product_alt_text='$product_alt_text',fabric='$fabric',product_image='$product_image'
         WHERE product_id=$id";
         $upload = mysqli_query($con,$update);
         if($upload){
             move_uploaded_file($product_image_tmp_name, $product_image_folder);
         }
         else{
-            $message[]='could not add new product';
+            $message[]='could not update product';
         }
 
     }
@@ -71,18 +73,19 @@ if (isset($_POST['update_product'])) {
 
                 <form class="card-add-form" action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">
                     <h2 style="color:white;">UPDATE</h2>
-                    <input type="text" class="login-input" placeholder="product name" value="<?php $row['product_name']; ?>" name="product_name" >
-                    <input type="number" class="login-input" placeholder="product price" value="<?php $row['product_price']; ?>" name="product_price" >
-                    <input type="number" class="login-input" placeholder="No Discount price" value="<?php $row['No_Discount_price']; ?>" name="No_Discount_price" >
+                    <input type="text" class="update-input" placeholder="product name" value="<?php echo $row['product_name']; ?>" name="product_name" >
+                    <input type="number" class="update-input" placeholder="product price" value="<?php echo $row['product_price']; ?>" name="product_price" >
+                    <input type="number" class="update-input" placeholder="No Discount price" value="<?php echo $row['No_Discount_price']; ?>" name="No_Discount_price" >
                     
                     
-
-                    <input type="text" class="login-input" placeholder="product description" value="<?php $row['product_description']; ?>" name="product_description" >
-                    <input type="text" class="login-input" placeholder="product alt text" value="<?php $row['product_alt_text']; ?>" name="product_alt_text" >
+                    <textarea form="formmmm" cols="66" style="white-space: pre-wrap;" id="product_description" value="<?php echo $row['product_description']; ?>" name="product_description" class="update-input prod-desc" required></textarea>
+                    <!-- <input type="text" class="update-input" placeholder="product description" value="" name="product_description" > -->
+                    <input type="text" class="update-input" placeholder="product alt text" value="<?php echo $row['product_alt_text']; ?>" name="product_alt_text" >
+                    <input type="text" id="fabric" name="fabric" value="<?php echo $row['fabric'] ?>" class="update-input" required>
                     <div class="choose-file-div">
-                        <input type="file" class="choose-file" accept="image/png,image/jpeg,image/jpg" name="product_image" >
+                        <input type="file" class="choose-file" accept="image/png,image/jpeg,image/jpg" name="product_image" required >
                     </div>
-                    <input type="submit" class="add-prod-btn" name="update_product" value="update product">
+                    <input type="submit" class="update-prod-btn" name="update_product" value="update product">
                     <a href="all_products.php" class="go-back-update">go back</a>
 
 
--- CLOTHING BRAND WEBSITE/ADMIN PANEL/products_update.php
+++ CLOTHING BRAND WEBSITE/ADMIN PANEL/products_update.php
@@ -14,6 +14,7 @@ if (isset($_POST['update_product'])) {
     $No_Discount_price = $_POST['No_Discount_price'];
     $product_description = $_POST['product_description'];
     $product_alt_text = $_POST['product_alt_text'];
+    $fabric = $_POST['fabric'];
     $product_image = $_FILES['product_image']['name'];
     $product_image_tmp_name = $_FILES['product_image']['tmp_name'];
     $product_image_folder = 'uploaded_img/' . $product_image;
@@ -24,14 +25,14 @@ if (isset($_POST['update_product'])) {
        $message[]='please fill form';
     }
     else{
-        $update = "UPDATE `main_products` SET product_name='$product_name',product_price='$product_price',No_Discount_price='$No_Discount_price',product_description='$product_description',product_alt_text='$product_alt_text',product_image='$product_image'
+        $update = "UPDATE `main_products` SET product_name='$product_name',product_price='$product_price',No_Discount_price='$No_Discount_price',product_description='$product_description',product_alt_text='$product_alt_text',fabric='$fabric',product_image='$product_image'
         WHERE product_id=$id";
         $upload = mysqli_query($con,$update);
         if($upload){
             move_uploaded_file($product_image_tmp_name, $product_image_folder);
         }
         else{
-            $message[]='could not add new product';
+            $message[]='could not update product';
         }
 
     }
@@ -50,7 +51,7 @@ if (isset($_POST['update_product'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="admin_update.css">
     <style>
-        <?php include "admin_update.css" ?>
+        <?php include "../css/admin_update.css" ?>
     </style> 
     <title>EDIT</title>
 </head>
@@ -69,20 +70,22 @@ if (isset($_POST['update_product'])) {
 
             ?>
 
-                <form class="card-add-form" action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">
+                <form class="card-add-form" id="formmmm" action="<?php $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">
                     <h2 style="color:white;">UPDATE</h2>
-                    <input type="text" class="login-input" placeholder="product name" value="<?php $row['product_name']; ?>" name="product_name" >
-                    <input type="number" class="login-input" placeholder="product price" value="<?php $row['product_price']; ?>" name="product_price" >
-                    <input type="number" class="login-input" placeholder="No Discount price" value="<?php $row['No_Discount_price']; ?>" name="No_Discount_price" >
+                    <input type="text" class="update-input" placeholder="product name" value="<?php echo $row['product_name']; ?>" name="product_name" >
+                    <input type="number" class="update-input" placeholder="product price" value="<?php echo $row['product_price']; ?>" name="product_price" >
+                    <input type="number" class="update-input" placeholder="No Discount price" value="<?php echo $row['No_Discount_price']; ?>" name="No_Discount_price" >
                     
                     
+                    <textarea form="formmmm" cols="66" style="white-space: pre-wrap;" id="product_description" value="<?php echo $row['product_description']; ?>" name="product_description" class="update-input prod-desc" required><?php echo $row['product_description'] ?></textarea>
+                    <!-- <input type="text" class="update-input prod-desc" placeholder="product description" value="" name="product_description" > -->
+                    <input type="text" class="update-input" placeholder="product alt text" value="<?php echo $row['product_alt_text']; ?>" name="product_alt_text" >
+                    <input type="text" id="fabric" name="fabric" value="<?php echo $row['fabric'] ?>" class="update-input" required>
 
-                    <input type="text" class="login-input" placeholder="product description" value="<?php $row['product_description']; ?>" name="product_description" >
-                    <input type="text" class="login-input" placeholder="product alt text" value="<?php $row['product_alt_text']; ?>" name="product_alt_text" >
                     <div class="choose-file-div">
-                        <input type="file" class="choose-file" accept="image/png,image/jpeg,image/jpg" name="product_image" >
+                        <input type="file" class="choose-file" accept="image/png,image/jpeg,image/jpg" name="product_image" required>
                     </div>
-                    <input type="submit" class="add-prod-btn" name="update_product" value="update product">
+                    <input type="submit" class="update-prod-btn" name="update_product" value="update product">
                     <a href="ADD_PRODUCT.php" class="go-back-update">go back</a>
 
 
